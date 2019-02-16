01b99235493b4bda2382cdde49141d8c96073e3b
Haris Tyagi <tyagiharis370@gmail.com>
2019-02-16 21:47:18 +0530

    initial commit

---
+++ database/migrations/2019_02_16_144631_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('buyer_id');
+            $table->date('order_date');
+            $table->date('required_date');
+            $table->date('shipped_date');
+            $table->unsignedInteger('shipper_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
